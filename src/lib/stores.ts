f469f6a6415daf3e748a4e60a138fec656270949
Dumitru-Daniel Mititelu <mttl_daniel@yahoo.com>
2022-04-17 20:50:47 +0300

    Comment localStorage issue

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,7 +1,10 @@
 import { writable } from "svelte/store";
 
-const storedPlayerName = localStorage.getItem("playerName");
-export const playerName = writable(storedPlayerName ?? "Anonymous");
-playerName.subscribe(name => {
-    localStorage.setItem("playerName", name);
-});
+// const storedPlayerName = localStorage.getItem("playerName");
+export const playerName = writable("Anonymous");
+// todo: there appear to be a bug with ssr rendering and localStorage
+// figure this out later.
+// playerName.subscribe(name => {
+//     localStorage.setItem("playerName", name);
+// });
+
